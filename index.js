4a67b4ed2b4b1bfc9cf1f5c420715f77a0b08364
Vania <aktives685@gmail.com>
2024-02-18 17:00:31 +0200

    practice JS

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-// import { name } from "./module";
-// import "./style.css";
-// // console.log("your name:" + name);
+import { name } from "./module";
+import "./style.css";
 
-// import isEmail from "validator/es/lib/isEmail";
-// import isURL from "validator/es/lib/isURL";
-
-// console.log(isEmail("foo@bar.com")); //=> true
-// console.log(isURL("sdfsdv")); //=>
+import isEmail from "validator/es/lib/isEmail";
+import isURL from "validator/es/lib/isURL";
+let socket = new WebSocket(
+  '"wss://javascript.info/article/websocket/demo/hello"'
+);
+socket.onopen = function (e) {
+  console.log("connect");
+  socket.send("hey");
+};
+socket.onmessage = function (e) {
+  console.log(e.data);
+};
+socket.onclose = function (event) {
+  if (event.wasClear) {
+    console.log("stopped ", event.code);
+  } else {
+    console.log("disconnect");
+  }
+};
+socket.onerror = function (err) {
+  console.log(err);
+};
